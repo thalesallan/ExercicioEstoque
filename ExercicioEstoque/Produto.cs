cf2ff24aa617b5cc609fda1206ee4288f7c02d7d
thalesallan <trabalhecomthales@gmail.com>
2021-05-11 09:09:15 -0300

    Adicionado metodo AdicionarProduto e RemoverProduto

--- ExercicioEstoque/Produto.cs
+++ ExercicioEstoque/Produto.cs
@@ -14,11 +14,22 @@ namespace ExercicioEstoque
             return Preco * Quantidade;
         }
 
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
         public override string ToString()
         {
             return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " Unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
+
 
     }
 }
